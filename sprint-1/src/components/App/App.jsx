ee676ec30d4ac0602e89f360711fde8c2d54f6c5
johnray-martinez <jrvmartinez@icloud.com>
2020-02-15 12:50:53 -0500

    finished desktop layout for video component. (Removed redundandant component [MainVideo])

--- sprint-1/src/components/App/App.jsx
+++ sprint-1/src/components/App/App.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../Header/Header';
 import NextVideoList from '../NextVideoList/NextVideoList';
-import MainVideo from '../MainVideo/MainVideo';
+import Video from '../Video/Video';
 import './App.scss';
-import mainVideo from '../../assets/Imports/mainVideo';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {mainVideoId: mainVideo.id}
-  }
-
   render() {
     return (
       <main>
         <Header />
-        <MainVideo  />
-        <NextVideoList mainVideoId={this.state.mainVideoId}/>
+        <Video  />
       </main>
     );
   }
--- sprint-1/src/components/Video/Video.jsx
+++ sprint-1/src/components/Video/Video.jsx
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import VideoPlayer from '../VideoPlayer/VideoPlayer';
+import CommentSection from '../CommentSection/CommentSection';
+import NextVideoList from '../NextVideoList/NextVideoList';
 import views from '../../assets/Icons/SVG/Icon-views.svg';
 import likes from '../../assets/Icons/SVG/Icon-likes.svg';
+import mainVideo from '../../assets/Imports/mainVideo';
 import './Video.scss';
 
-function Video(props){
-    let year = new Date(props.mainVideo.timestamp).getFullYear();
-    let month = new Date(props.mainVideo.timestamp).getMonth();    
-    let day = new Date(props.mainVideo.timestamp).getDate();
-    return (
-        <section className="video">
-            <VideoPlayer video={props.mainVideo} />
-            <div className="video__details">
-                <h2 className="video__title">{props.mainVideo.title}</h2>
-                <div className="video__blurb">
-                    <div className="video__details--flex">
-                        <p className="video__channel"><strong>{props.mainVideo.channel}</strong></p>
-                        <span className="video__timestamp">{`${month}/${day}/${year}`}</span>
-                    </div>  
-                    <div className="video__stats-container">
-                        <span className="video__stats">
-                            <img className="video__stat-icon" src={views} alt="views icon"/>
-                            <span className="video__stat-value"> {props.mainVideo.views} </span>
-                        </span>
-                        <span className="video__stats">
-                            <img className="video__stat-icon" src={likes} alt="likes icon"/>
-                            <span className="video__stat-value"> {props.mainVideo.likes} </span>
-                        </span>
+class Video extends Component {
+    state = { mainVideo }
+    year = new Date(this.state.mainVideo.timestamp).getFullYear();
+    month = new Date(this.state.mainVideo.timestamp).getMonth();    
+    day = new Date(this.state.mainVideo.timestamp).getDate();
+    
+    render() {
+        return (
+            <section className="video">
+                <VideoPlayer video={this.state.mainVideo} />
+                <div class="video__main-container">
+                    <div className="video__details">
+                        <h2 className="video__title">{this.state.mainVideo.title}</h2>
+                        <div className="video__blurb">
+                            <div className="video__details--flex">
+                                <p className="video__channel"><strong>{this.state.mainVideo.channel}</strong></p>
+                                <span className="video__timestamp">{`${this.month}/${this.day}/${this.year}`}</span>
+                            </div>  
+                            <div className="video__stats-container">
+                                <span className="video__stats">
+                                    <img className="video__stat-icon" src={views} alt="views icon"/>
+                                    <span className="video__stat-value"> {this.state.mainVideo.views} </span>
+                                </span>
+                                <span className="video__stats">
+                                    <img className="video__stat-icon" src={likes} alt="likes icon"/>
+                                    <span className="video__stat-value"> {this.state.mainVideo.likes} </span>
+                                </span>
+                            </div>
+                        </div>
+                        <hr />
+                        <p className="video__description"> {this.state.mainVideo.description} </p>
+                        <CommentSection comments={this.state.mainVideo.comments}/>
                     </div>
+                    <NextVideoList mainVideoId={this.state.mainVideoId}/>
                 </div>
-                <hr />
-                <p className="video__description"> {props.mainVideo.description} </p>
-            </div>
-        </section>
-    );
+            </section>
+        );
+    }
 }
 
 export default Video;
