ecb43db9d50150ad55f611e15b6a2de5334c6fcd
johnray-martinez <jrvmartinez@icloud.com>
2020-02-16 18:35:58 -0500

    implemented function to change the middle icon and display it when changing the playState of the VideoPlayer component

--- sprint-1/src/components/VideoPlayer/VideoPlayer.jsx
+++ sprint-1/src/components/VideoPlayer/VideoPlayer.jsx
@@ -6,37 +6,64 @@ import pause from '../../assets/Icons/SVG/Icon-pause.svg';
 import volume from '../../assets/Icons/SVG/Icon-volume.svg';
 import fullscreen from '../../assets/Icons/SVG/Icon-fullscreen.svg';
 function VideoPlayer(props){
-    // todos: add setTimeout to add delay to the controls when transitioning to hidden again
-    // add icon in the middle to see what is the playState of the video
-    // controls should autohide after 3 secs after mouse stops moving when in full screen mode
+    // todos: add setTimeout to add delay to the controls when transitioning to hidden again *** FIXED ***
+    // add icon in the middle to see what is the playState of the video *** FIXED ***
+    // controls should autohide after 3 secs after mouse stops moving *** FIXED ***
     // fix the bug where pressing spacebar while in input texts changes the playState *** FIXED ***
     // fix the bug where playState changes even when clicking full screen buttons or volume *** FIXED ***
     // fix duration of video
     
     // EVENT LISTENER TO CHANGE ICON WHEN PRESSING SPACEBAR OR WHEN CLICKING THE VIDEO OR THE PLAY/PAUSE BUTTON
     let playState = "paused";
+    let timeoutId = 0;
+    let timeoutId2 = 0;
     const changeIcon = e => {
+        let playButton = document.querySelector('.video-player__play-button');
+        let middleIcon = document.querySelector('.video-player__middle-icon');
         e.stopPropagation();
-        if (e.target.classList[0] === 'video-player__play-button' || e.target.classList[0] === 'video-player__video' || (e.code === 'Space' && document.activeElement !== document.querySelector('textarea'))){
-            let playButton = document.querySelector('.video-player__play-button');
+        if (e.target === playButton ||
+            e.target === middleIcon || 
+            e.target.classList[0] === 'video-player__video' || 
+            (e.code === 'Space' && document.activeElement !== document.querySelector('textarea'))){
             if (playState === "paused") {
                 playState = "play";
                 playButton.src = pause;
+                middleIcon.src = pause;
             } else {
                 playState = "paused";
                 playButton.src = play;
+                middleIcon.src = play;
             }
+            clearTimeout(timeoutId2);
+            middleIcon.classList.add('visible');
+            timeoutId2 = setTimeout(()=>{
+                middleIcon.classList.remove('visible'); 
+                timeoutId2 = 0;
+            },400)
         }
     }
+
+    // callback that changes the playState to pause or play when pressing spacebar  (for now just changes icon)
     document.addEventListener("keypress", e=>{
         if (e.keyCode === 32 && e.target === document.body)
             e.preventDefault(); // to prevent spacebar from scrolling the page
             changeIcon(e);
     }) 
 
+    // callback that shows video controls briefly upon mousemove on video-player
+    const showControls = e => {
+        clearTimeout(timeoutId);
+        let controls = document.querySelector('.video-player__controls');
+        controls.classList.add('visible');
+        timeoutId = setTimeout(()=>{
+            controls.classList.remove('visible');
+            timeoutId = 0;
+        },2000);
+    }
+
     return(
-        <div className="video-player" onClick={changeIcon} >
-          <video className="video-player__video" poster={props.video.image} ></video>
+        <div className="video-player" onClick={changeIcon} onMouseMove={showControls} >
+            <video className="video-player__video" poster={props.video.image} ></video>
             <div className="video-player__controls">
                 <img alt="play button" onClick={changeIcon} src={play} className="video-player__play-button" /> 
                 <div className="video-player__scrubber-bar"> 
@@ -48,6 +75,7 @@ function VideoPlayer(props){
                     <img alt="volume" src={volume} className="video-player__volume" /> 
                 </div>
             </div>
+            <img className="video-player__middle-icon" src={play} onClick={changeIcon}></img>
         </div>
     );
 }
