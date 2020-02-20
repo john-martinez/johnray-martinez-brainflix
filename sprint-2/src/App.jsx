89764a5da59e48eef7bad5a9619bc016aed407bd
johnray-martinez <jrvmartinez@icloud.com>
2020-02-20 15:42:23 -0500

    finished structure and mobile view of Upload component

--- sprint-2/src/App.jsx
+++ sprint-2/src/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Video from './pages/Video/Video';
 import NotFound from './pages/NotFound/NotFound';
+import Upload from './pages/Upload/Upload'; // THIS IS WHERE YOU STOPPED JOHN YOU ARE REALLY AWESOME
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 
@@ -12,6 +13,7 @@ class App extends Component {
         <Header />
           <Switch>
             <Route exact path="/" render={props=><Video {...props}/>} />
+            <Route exact path="/upload" component={Upload} />
             <Route exact path="/:videoId" component={Video}/>
             <Route component={NotFound} />
           </Switch>
--- sprint-2/src/pages/Upload/Upload.jsx
+++ sprint-2/src/pages/Upload/Upload.jsx
@@ -1,1 +1,28 @@
 import React from 'react';
+import videoPreviewImg from '../../assets/Images/Upload-video-preview.jpg';
+import './Upload.scss';
+
+export default function Upload() {
+    return (
+        <section className="upload">
+        <h1 className="upload__header">Upload Video</h1>
+        <form action="">
+            <div className="upload__item">
+                <h4 className="upload__item-header">Video Thumbnail</h4>    
+                <img className="upload__video" src={videoPreviewImg} alt="blue bike"/>
+            </div>
+            <div className="upload__item">
+                <h4><label className="upload__item-header" htmlFor="title">TITLE OF YOUR VIDEO</label></h4>
+                <input className="upload__input" type="text" name="title" placeholder="Add a title to your video" />
+            </div>
+            <div className="upload__item">
+                <h4><label className="upload__item-header" htmlFor="description">ADD A VIDEO DESCRIPTION</label></h4>
+                <textarea className="upload__input textarea" name="description" placeholder="Add a description of your video" ></textarea>
+            </div>
+            <button className="upload__button">PUBLISH</button>
+            <button className="upload__button--inverted">CANCEL</button>
+        </form>
+        </section>
+    );
+}
+
