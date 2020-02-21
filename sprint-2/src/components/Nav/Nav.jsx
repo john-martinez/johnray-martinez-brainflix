d6293229f24f63ae7a00959a99235716e842f3b1
johnray-martinez <jrvmartinez@icloud.com>
2020-02-20 23:42:30 -0500

    fixed links for nextVideo when clicked

--- sprint-2/src/components/Nav/Nav.jsx
+++ sprint-2/src/components/Nav/Nav.jsx
@@ -3,13 +3,14 @@ import logo from '../../assets/Logo/Logo-brainflix.svg';
 import user from '../../assets/Images/Mohan-muruge.jpg';
 import search from '../../assets/Icons/SVG/Icon-search.svg';
 import upload from '../../assets/Icons/SVG/Icon-upload.svg';
+import { Link } from 'react-router-dom';
 import './Nav.scss';
 
 function Nav () {
   return (
     <nav className="nav">
       <div className="nav__left"> 
-        <img className="nav__logo" src={logo} alt="brainflix logo" />
+        <Link to="/" className="nav__logo"> <img className="nav__logo" src={logo} alt="brainflix logo" /> </Link>
       </div>
       <div className="nav__right">
         <div className="nav__search-container"> 
@@ -17,11 +18,13 @@ function Nav () {
             <img src={search} className="nav__search-icon" alt="magnifying glass"></img>
           </div>
           <div className="nav__actions">
+            <Link to="/upload" className="nav__button">
               <button className="nav__button"> 
                 <img className="nav__upload-icon" src={upload}  alt="upload icon" /> 
                 <span className="nav__uload-text">UPLOAD</span> 
               </button>
-              <img src={user} className="nav__user" alt="Mohan Murugi side view"></img>
+            </Link>
+            <img src={user} className="nav__user" alt="Mohan Murugi side view"></img>
           </div>
         </div>
     </nav>
