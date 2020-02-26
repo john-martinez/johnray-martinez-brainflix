7c299c00815f3a5d936eb33a96b120244eda09c3
johnray-martinez <jrvmartinez@icloud.com>
2020-02-25 20:15:42 -0500

    added validation and upload endpoint for upload component

--- sprint-3/client/src/pages/Upload/Upload.jsx
+++ sprint-3/client/src/pages/Upload/Upload.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 import videoPreviewImg from '../../assets/images/Upload-video-preview.jpg';
 import './Upload.scss';
 
+const LINK = 'http://localhost:8080';
+const PATH = '/videos';
+
 export default function Upload() {
+    const onSubmitHandler = e => {
+        e.preventDefault();
+        let { title } = e.target;
+        title.classList.remove('wrong-input');
+        if (title.value.trim()) {
+            axios.post(`${LINK}${PATH}`, {
+                title: title.value, 
+                channel: "BrainStation Guy",
+                image: "https://i.imgur.com/yFS8EBr.jpg"
+            })
+            .then(res=>console.log(res.data))
+            .catch(err=>console.log(err))
+        } else {
+            title.classList.add('shake');
+            title.classList.add('wrong-input');
+            setTimeout(()=>title.classList.remove('shake'), 200)
+        }
+        e.target.reset();
+    }
     return (
         <section className="upload">
             <h1 className="upload__header">Upload Video</h1>
-            <form action="">
+            <form action="" onSubmit={onSubmitHandler}>
                 <div className="upload__main">
                     <div className="upload__flex-container">
                         <div className="upload__left">
